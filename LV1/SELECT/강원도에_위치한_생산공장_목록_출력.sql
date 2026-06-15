6c56ab459dd20356c860faad503e66d6bc255900
Haemin Cho/조해민 <chohaemin123@gmail.com>
2026-06-15 23:46:17 +0900

    Update 강원도에_위치한_생산공장_목록_출력.sql

--- LV1/SELECT/강원도에_위치한_생산공장_목록_출력.sql
+++ LV1/SELECT/강원도에_위치한_생산공장_목록_출력.sql
@@ -1,16 +1,18 @@
-# 강원도에 위치한 생산공장 목록 출력
+-- 문제 : 강원도에 위치한 생산공장 목록 출력하기
 
-## 문제
-FOOD_FACTORY 테이블에서 강원도에 위치한 식품공장의 공장 ID, 공장 이름, 주소를 조회한다.
-결과는 공장 ID 기준 오름차순으로 정렬한다.
+-- 사용 문법
+-- LIKE : 특정 문자열 패턴 검색
+-- ORDER BY : 정렬
+-- ASC : 오름차순 정렬
 
-## 풀이
-- ADDRESS 컬럼이 '강원도'로 시작하는 데이터만 조회
-- LIKE '강원도%' 조건 사용
-- FACTORY_ID 기준 오름차순 정렬
+-- 배운 점
+-- 주소가 특정 지역으로 시작하는지 확인할 때 LIKE를 사용할 수 있다.
+-- '%'는 와일드카드로 뒤에 어떤 문자열이 와도 허용한다.
+-- ORDER BY를 사용하여 원하는 기준으로 결과를 정렬할 수 있다.
 
-## SQL
-SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+SELECT FACTORY_ID,
+       FACTORY_NAME,
+       ADDRESS
 FROM FOOD_FACTORY
 WHERE ADDRESS LIKE '강원도%'
 ORDER BY FACTORY_ID ASC;
